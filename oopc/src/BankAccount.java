81a8e1d45450375a5ffbed567a2fec4dc7adc4e6
rakins <shahriar.rakin99@gmail.com>
2024-05-18 16:31:39 +0600

    used two different cls

--- oopc/src/BankAccount.java
+++ oopc/src/BankAccount.java
@@ -1,17 +1,23 @@
-public class BankAccount {
-    public static void main(String args[]){
-        BankAccount raks = new BankAccount();
-        raks.balance = 1000;
-        raks.withdraw(200);
-        System.out.println(raks.balance);
-    }
+
+class BankAcc {
+
 
     public String name;
     public String id;
     public double balance;
 
-    public void withdraw(double amount){
+    public void withdraw(double amount) {
         balance = balance - amount;
     }
 
 }
+
+public class BankAccount{
+    public static void main(String args[]){
+        BankAcc raks = new BankAcc();
+        raks.balance = 1000;
+        raks.withdraw(200);
+        System.out.println(raks.balance);
+    }
+}
+
