ace8d43f181fe78b5c8afae0cc8e6d8a92d6cdff
rakins <shahriar.rakin99@gmail.com>
2024-05-18 12:34:40 +0600

    foreach loop

--- oopc/src/Array.java
+++ oopc/src/Array.java
@@ -2,8 +2,12 @@ public class Array {
     public static void main(String args[]){
         int arr[] = new int[10];
 
-        for (int i= 0; i< arr.length; i++){
-            System.out.println(arr[i]);
+//        for (int i= 0; i< arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+//
+        for(int j : arr){
+            System.out.println(j);
         }
     }
 }
