2f2f29d945e08620a02ca932b5f12bf5d0813ade
rakins <shahriar.rakin99@gmail.com>
2024-05-18 11:31:10 +0600

    casting

--- oopc/src/HelloWorld.java
+++ oopc/src/HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World");
+
+        //casting
+        short s1 = 1000;
+        int i1 = s1;    // auto casting
+
+        int i2 = 1000;
+        short s2 = (short)i2;    // explicit casting
+
+        float f1 = 10.5f;     // casting 10.5 (which is, by default, a double) to a float
+
+        float f2 = 10.2f;
+        int i3 = (int)f2;     // casting float to int
+
+        long l1 = 10000000L;       // casting 10000000 (which is, by default, a int) to a long
+
+
+        // summary - kono number k float korte last e f dite hoy
+        // kono number k long korte hole last e L dite hoy
     }
 }
