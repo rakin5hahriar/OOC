7a0e86dae85ff493f4dc4be070a7e753489ab4e9
rakins <shahriar.rakin99@gmail.com>
2024-05-18 12:25:23 +0600

    array init and print

---
+++ oopc/src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String args[]){
+        int arr[] = new int[10];
+
+        for (int i= 0; i< arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
