9e77e3eebaac21875e58e766c4ac8f43aad136a2
rakins <shahriar.rakin99@gmail.com>
2024-05-19 11:26:18 +0600

    pass by value

---
+++ oopc/src/SwapperMain.java
@@ -0,0 +1,26 @@
+public class SwapperMain {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        // Create an instance of the Swapper class
+        Swapper swapper = new Swapper();
+
+        // Call the swap method on the Swapper instance
+        swapper.swapValues(a, b);
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
+
+
+class Swapper {
+    // Method to swap two integers
+    public void swapValues(int x, int y) {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+}
---
+++ oopc/src/ValueHolderMain.java
@@ -0,0 +1,34 @@
+class ValueHolder {
+    // Attributes
+    public int value;
+
+    // Constructor
+    public ValueHolder(int value) {
+        this.value = value;
+    }
+
+    // Static method to swap values of two ValueHolder objects
+    public static void swapValues(ValueHolder a, ValueHolder b) {
+        int temp = a.value;
+        a.value = b.value;
+        b.value = temp;
+    }
+}
+
+
+public class ValueHolderMain {
+    public static void main(String[] args) {
+        // Create two ValueHolder objects
+        ValueHolder holder1 = new ValueHolder(5);
+        ValueHolder holder2 = new ValueHolder(10);
+
+        // Display values before swapping
+        System.out.println("Before swap: holder1 = " + holder1.value + ", holder2 = " + holder2.value);
+
+        // Swap the values using the static method in ValueHolder class
+        ValueHolder.swapValues(holder1, holder2);
+
+        // Display values after swapping
+        System.out.println("After swap: holder1 = " + holder1.value + ", holder2 = " + holder2.value);
+    }
+}
